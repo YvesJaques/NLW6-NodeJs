09aadf68e570f37384a595a5143b11467b2ec750
Yves Morais Jaques <ymoraisjaques@gmail.com>
2021-06-25 08:32:22 -0300

    feat User AUthentication

--- routes.ts
+++ routes.ts
@@ -1,14 +1,18 @@
 import { Router } from "express";
+import { AuthenticateUserController } from "./src/controllers/AuthenticateUserController";
 import { CreateTagController } from "./src/controllers/CreateTagController";
 import { CreateUserController } from "./src/controllers/CreateUserController";
+import { ensureAdmin } from "./src/middlewares/ensureAdmin";
 
 
 const router = Router();
 
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
+const authenticateUserController = new AuthenticateUserController();
 
+router.post("/tags", ensureAdmin, createTagController.handle);
 router.post("/users", createUserController.handle);
-router.post("/tags", createTagController.handle);
+router.post("/login", authenticateUserController.handle)
 
 export { router }
--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,14 +1,16 @@
+import { hash } from "bcryptjs";
 import { getCustomRepository } from "typeorm";
 import { UsersRepository } from "../repositories/UsersRepository";
 
 interface IUserRequest {
     name: string;
     email: string;
-    admin?: boolean
+    admin?: boolean;
+    password: string;
 }
 
 class CreateUserService {
-    async execute({ name, email, admin} : IUserRequest) {
+    async execute({ name, email, admin, password} : IUserRequest) {
         const usersRepository = getCustomRepository(UsersRepository);
 
         if(!email) {
@@ -23,10 +25,13 @@ class CreateUserService {
             throw new Error("User already exists!")
         }
 
+        const passwordHash = await hash(password,8);
+
         const user = usersRepository.create({
             name,
             email,
-            admin
+            admin,
+            password: passwordHash
         })
 
         await usersRepository.save(user);
